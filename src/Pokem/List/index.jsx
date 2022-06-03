a31020265ae9bd322ad62e2bfaae106dbbfe06f2
Henrique Rodrigues <79226722+Krissafff@users.noreply.github.com>
2022-06-03 13:46:40 -0300

    att.3

--- src/Pokem/List/index.jsx
+++ src/Pokem/List/index.jsx
@@ -1,7 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import './style.css'
+
 const PokemonList = () => {
-  const [pokemons, setPokemons] = useState(null);
+  const [pokemons, setPokemons] = useState([]);
+  const [itensPerPage, setItensPerPage] = useState(5);
+  const [currentPage, setCurrentPage] = useState(0);
+
+  const pages = Math.ceil(pokemons.length / itensPerPage)
+  const startIndex = currentPage * itensPerPage;
+  const endIndex = startIndex + itensPerPage;
+  const currentItens = pokemons.slice(startIndex, endIndex)
 
   useEffect(() => {
     fetch('https://pokeapi.co/api/v2/pokemon/')
@@ -16,8 +25,19 @@ const PokemonList = () => {
   }
 
   return (
+    <>
+    <div>
+      <select value={itensPerPage} onChange={(e) => setItensPerPage(Number(e.target.value))}>
+        <option value={5}>5</option>
+        <option value={10}>10</option>
+        <option value={20}>20</option>
+      </select>
+      {Array.from(Array(pages), (item, index) => {
+        return <button value={index} onClick={(e) => setCurrentPage(Number(e.target.value))}>{index + 1}</button>
+      })}
+    </div>
     <ul className="PokemonList">
-      {pokemons.map(({ name }) => (
+      {currentItens.map(({ name }) => (
         <li key={name}>
           <Link to={`/pokemons/${name}`}>
             {name}
@@ -25,6 +45,7 @@ const PokemonList = () => {
         </li>
       ))}
     </ul>
+    </>
   );
 };
 
